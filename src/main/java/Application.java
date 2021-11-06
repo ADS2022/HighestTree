f83fabf991cd36e9f84e86a5f161ef2156f45008
lfpneto <lfpneto@gmail.com>
2021-11-06 03:29:41 +0000

    User Edit or View mode
    Application.java
    - Ask user if wants to view or edit
    - Get input
    - Update editMode flag
    - DEFECT: in input is not a digit --> crash.
    
    README.md
    - Duplicated readme files. I would like to have a the readme file in the master branch so that we can edit it at the same time as the Application. SADRA, can we delete the other one or is it required to have a README.md in 'main' branch?

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,46 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("Helloooooooooooooooooooooo World!");
+
+
+
+        // User selects View or Edit mode
+        Boolean userLoop = true;
+        Boolean editMode = false;
+        int view = 1;
+        int edit = 2;
+
+        while (userLoop){
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Select a mode:");
+            System.out.println("View (" + view + ") | Edit (" + edit + ")");
+
+            // Retrieve the user's input
+            // DEFECT : if input not in [0-9] interval, there is an error
+            int input = scanner.nextInt();
+            try {
+                if ( true ) {
+                    if (input == view ){
+                        System.out.println("You have selected " + view);
+                        userLoop = false;
+                    } else if ( input == edit ){
+                        System.out.println("You have selected " + edit);
+                        editMode = true;
+                        userLoop = false;
+                    }
+                } else {
+                    System.out.println("ONE or TWO!!! ٩(º_º)۶");
+                }
+            } catch (InputMismatchException e){
+                System.out.println("ONE or TWO!!! ٩(º_º)۶ !!! No characters allowed !!!");
+            }
+
+        }//userLoop = 0
+
+
     }
 }
---
+++ src/main/java/Requirements.md
@@ -0,0 +1,5 @@
+#Requirements
+
+[Link google docs](https://docs.google.com/spreadsheets/d/1xZ9EhlMW8qUNATcF684Nj9N8olOkkjLRqoa5ysPpLKs/edit?usp=sharing)
+
+
