10d7e276dbcefb6435ba06da45a275fb3b59bbe4
lfpneto <lfpneto@gmail.com>
2021-11-29 23:36:28 +0000

    JavaFX - First atempt at a landing menu. PLEASE FIX changing between scenes: When button "Person Menu", ERROR path to file .fxml

--- src/main/java/mesw/ads/highesttreemaven/HelloApplication.java
+++ src/main/java/mesw/ads/highesttreemaven/HelloApplication.java
@@ -1,7 +1,9 @@
 package mesw.ads.highesttreemaven;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -14,12 +16,26 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        //WORKING
         //Javafx window title
         stage.setTitle("HighestTree");
 
-        FXMLLoader window = new FXMLLoader(HelloApplication.class.getResource("HomeView.fxml"));
-        Scene HomeView = new Scene(window.load());
-        stage.setScene(HomeView);
+        FXMLLoader root = new FXMLLoader(HelloApplication.class.getResource("HomeView.fxml"));
+        Scene scene = new Scene(root.load());
+        stage.setScene(scene);
         stage.show();
+
+         /*
+
+        Parent root = FXMLLoader.load(getClass().getResource("HomeView.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();      */
+
+    }
+
+    public void goToPersonsMenu(ActionEvent actionEvent) {
+
+
     }
 }
--- src/main/java/mesw/ads/highesttreemaven/HighestTree/controller/HomeController.java
+++ src/main/java/mesw/ads/highesttreemaven/HighestTree/controller/HomeController.java
@@ -1,19 +1,54 @@
 package mesw.ads.highesttreemaven.HighestTree.controller;
 
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import mesw.ads.highesttreemaven.HelloApplication;
 
-public class HomeController {
-    public Button btnPersonsMenu, btnEventsMenu, btnPlacesMenu;
 
-        public void goToPersonMenu(ActionEvent actionEvent) {
-        System.out.println("Change to PersonMenu solicited");
-        
+public class HomeController {
+    @FXML
+    private Button btnPersonsMenu, btnEventsMenu, btnPlacesMenu;
+
+    public void goToPersonsMenu(ActionEvent actionEvent) throws IOException {
+
+        //HERE WE ARE TRYING TO DEBUG THE PATH TO .FXML FILE
+        /*HomeController shit = new HomeController();
+        Class class1 = shit.getClass();
+        URL url2 = class1.getResource("HomeController.java");
+        System.out.println("Value URL = " + url2);*/
+
+
+
+        if(actionEvent.getSource()==btnPersonsMenu){
+            System.out.println("Changing to Persons scene");
+            Stage stage = (Stage) btnPersonsMenu.getScene().getWindow();
+
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("/PersonsView.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        else if (actionEvent.getSource()==btnEventsMenu){
+            System.out.println("Changing to Events scene");
+
+        }
+        else if (actionEvent.getSource()==btnPlacesMenu){
+            System.out.println("Changing to Places scene");
+
+        }
 
     }
+
+
 }
