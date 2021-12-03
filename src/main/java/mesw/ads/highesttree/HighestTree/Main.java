680a4d22eba91bda7b65738f7d2fede92dfd0d87
Sarah Schaab <s.schaab@campus.lmu.de>
2021-12-03 12:56:41 +0000

    merge

--- src/main/java/mesw/ads/highesttree/HighestTree/Main.java
+++ src/main/java/mesw/ads/highesttree/HighestTree/Main.java
@@ -6,14 +6,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
 
         //Javafx window title
         stage.setTitle("HighestTree");
-        FXMLLoader root = new FXMLLoader(HelloApplication.class.getResource("HomeView.fxml"));
+        FXMLLoader root = new FXMLLoader(Main.class.getResource("HomeView.fxml"));
         Scene scene = new Scene(root.load());
         stage.setScene(scene);
         stage.show();
